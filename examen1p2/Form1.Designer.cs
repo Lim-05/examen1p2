fd33675f7fef1ed5207b1e472ae6b876efe22487
Morales Sánchez Limber de Jesús <limbermorales13@outlook.com>
2024-11-07 21:10:27 -0600

    Agregar archivos de proyecto.

---
+++ examen1p2/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace examen1p2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lbFecha = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbFecha = new System.Windows.Forms.TextBox();
+            this.btGuardar = new System.Windows.Forms.Button();
+            this.lbCelcius = new System.Windows.Forms.Label();
+            this.lbFahrenheit = new System.Windows.Forms.Label();
+            this.btEncender = new System.Windows.Forms.Button();
+            this.btApagar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Location = new System.Drawing.Point(23, 29);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(44, 13);
+            this.lbNombre.TabIndex = 0;
+            this.lbNombre.Text = "Nombre";
+            // 
+            // lbFecha
+            // 
+            this.lbFecha.AutoSize = true;
+            this.lbFecha.Location = new System.Drawing.Point(23, 68);
+            this.lbFecha.Name = "lbFecha";
+            this.lbFecha.Size = new System.Drawing.Size(37, 13);
+            this.lbFecha.TabIndex = 1;
+            this.lbFecha.Text = "Fecha";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(73, 26);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(100, 20);
+            this.tbNombre.TabIndex = 2;
+            // 
+            // tbFecha
+            // 
+            this.tbFecha.Location = new System.Drawing.Point(73, 65);
+            this.tbFecha.Name = "tbFecha";
+            this.tbFecha.Size = new System.Drawing.Size(100, 20);
+            this.tbFecha.TabIndex = 3;
+            // 
+            // btGuardar
+            // 
+            this.btGuardar.Location = new System.Drawing.Point(73, 111);
+            this.btGuardar.Name = "btGuardar";
+            this.btGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btGuardar.TabIndex = 4;
+            this.btGuardar.Text = "Guardar";
+            this.btGuardar.UseVisualStyleBackColor = true;
+            // 
+            // lbCelcius
+            // 
+            this.lbCelcius.AutoSize = true;
+            this.lbCelcius.Location = new System.Drawing.Point(298, 29);
+            this.lbCelcius.Name = "lbCelcius";
+            this.lbCelcius.Size = new System.Drawing.Size(43, 13);
+            this.lbCelcius.TabIndex = 5;
+            this.lbCelcius.Text = "Celsius:";
+            // 
+            // lbFahrenheit
+            // 
+            this.lbFahrenheit.AutoSize = true;
+            this.lbFahrenheit.Location = new System.Drawing.Point(298, 72);
+            this.lbFahrenheit.Name = "lbFahrenheit";
+            this.lbFahrenheit.Size = new System.Drawing.Size(60, 13);
+            this.lbFahrenheit.TabIndex = 6;
+            this.lbFahrenheit.Text = "Fharenheit:";
+            // 
+            // btEncender
+            // 
+            this.btEncender.Location = new System.Drawing.Point(65, 231);
+            this.btEncender.Name = "btEncender";
+            this.btEncender.Size = new System.Drawing.Size(108, 23);
+            this.btEncender.TabIndex = 7;
+            this.btEncender.Text = "Encender LED";
+            this.btEncender.UseVisualStyleBackColor = true;
+            this.btEncender.Click += new System.EventHandler(this.btEncender_Click);
+            // 
+            // btApagar
+            // 
+            this.btApagar.Location = new System.Drawing.Point(250, 231);
+            this.btApagar.Name = "btApagar";
+            this.btApagar.Size = new System.Drawing.Size(108, 23);
+            this.btApagar.TabIndex = 8;
+            this.btApagar.Text = "Apagar LED";
+            this.btApagar.UseVisualStyleBackColor = true;
+            this.btApagar.Click += new System.EventHandler(this.btApagar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 318);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btApagar);
+            this.Controls.Add(this.btEncender);
+            this.Controls.Add(this.lbFahrenheit);
+            this.Controls.Add(this.lbCelcius);
+            this.Controls.Add(this.btGuardar);
+            this.Controls.Add(this.tbFecha);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.lbFecha);
+            this.Controls.Add(this.lbNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Label lbFecha;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbFecha;
+        private System.Windows.Forms.Button btGuardar;
+        private System.Windows.Forms.Label lbCelcius;
+        private System.Windows.Forms.Label lbFahrenheit;
+        private System.Windows.Forms.Button btEncender;
+        private System.Windows.Forms.Button btApagar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ examen1p2/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
+using System.IO.Ports;
+using System.Globalization;
+using Mysqlx.Cursor;
+
+namespace examen1p2
+{
+    public partial class Form1 : Form
+    {
+        private string SqlConection = "Server=localhost; Port=3306; Database=Exapractico1;Uid=root;Pwd=12345;";
+        public SerialPort ArduinoPort { get; }
+        private ToolTip toolTip;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tbNombre.TextChanged += validarNombre;
+            tbFecha.TextChanged += tbFecha_TextChanged;
+
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM7";
+            ArduinoPort.BaudRate = 9600;
+            //ArduinoPort.Open();
+            toolTip = new ToolTip();
+
+            // Vincular eventos
+            this.FormClosing += CerrandoForm1;
+            this.btApagar.Click += btApagar_Click;
+            this.btEncender.Click += btEncender_Click;
+        }
+
+        private void InsertarRegistro(string nombre, string fecha, decimal Temp_Celcius, decimal Temp_Fahrenheit)
+        {
+            try
+            {
+                using (MySqlConnection conection = new MySqlConnection(SqlConection))
+                {
+                    conection.Open();
+
+                    string insertQuery = "INSERT INTO datos (Nombre_usuario, Fecha, Temp_Celcius, Temp_Fahrenheit) " +
+                                         "VALUES (@Nombre_usuario, @Fecha, @Temp_Celcius, @Temp_Fahrenheit)";
+
+                    using (MySqlCommand command = new MySqlCommand(insertQuery, conection))
+                    {
+                        // Añadir el parámetro Nombre_usuario
+                        command.Parameters.AddWithValue("@Nombre_usuario", nombre);
+
+                        // Convertir la fecha a DateTime
+                        DateTime fechaDateTime;
+                        if (DateTime.TryParseExact(fecha, "yyyy/MM/dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out fechaDateTime))
+                        {
+                            // Añadir el parámetro Fecha como DateTime
+                            command.Parameters.AddWithValue("@Fecha", fechaDateTime.Date);
+                        }
+                        else
+                        {
+                            MessageBox.Show("El formato de fecha no es válido. Por favor usa el formato yyyy/MM/dd.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return; // Salir si la fecha no es válida
+                        }
+
+                        // Añadir los parámetros de temperatura
+                        command.Parameters.AddWithValue("@Temp_Celcius", Temp_Celcius);
+                        command.Parameters.AddWithValue("@Temp_Fahrenheit", Temp_Fahrenheit);
+
+                        // Ejecutar la consulta
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar los datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void tbFecha_TextChanged(object sender, EventArgs e)
+        {
+            if (!validarFecha(tbFecha.Text))
+            {
+                toolTip.SetToolTip(tbFecha, "Por favor ingresa una fecha válida (formato: yyyy/MM/dd).");
+            }
+            else
+            {
+                toolTip.SetToolTip(tbFecha, string.Empty);
+            }
+        }
+
+
+        private bool validarFecha(string Fecha)
+        {
+            return DateTime.TryParse(Fecha, out _);
+        }
+
+        private bool EsTextoValido(string valor)
+        {
+            return Regex.IsMatch(valor, @"^[a-zA-Z\s]+$");
+        }
+
+        private void validarNombre(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsTextoValido(textBox.Text))
+            {
+                MessageBox.Show("Ingrese un nombre válido", "Error Nombre", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void btGuardar_Click(object sender, EventArgs e)
+        {
+            // Obtener valores de entrada de los controles del formulario
+            string nombre = tbNombre.Text;
+            string fecha = tbFecha.Text;
+            string Temp_Celcius = lbCelcius.Text;
+
+            try
+            {
+                // Convertir la temperatura de Celsius a Fahrenheit
+                decimal tempCelcius = decimal.Parse(Temp_Celcius);
+                decimal tempFahrenheit = (tempCelcius * 9 / 5) + 32;
+
+                // Asignar valores fijos a las etiquetas para mostrar en el formulario
+                float lbCel = 25.0f; // Muestra un valor fijo de ejemplo
+                float lbFar = lbCel * 9 / 5 + 32;
+
+                // Llamar al método para insertar el registro en la base de datos
+                InsertarRegistro(nombre, fecha, tempCelcius, tempFahrenheit);
+
+                // Actualizar las etiquetas para mostrar la temperatura en Celsius y Fahrenheit
+                lbCelcius.Text = $"Celsius: {lbCel}°C"; // Mostrar en Celsius
+                lbFahrenheit.Text = $"Fahrenheit: {lbFar}°F"; // Mostrar en Fahrenheit
+
+                // Crear el mensaje de confirmación para el usuario
+                string usuario = $"Nombre: {nombre}\r\nFecha: {fecha}";
+
+                // Mostrar el mensaje de éxito
+                MessageBox.Show("Datos guardados con éxito: \n\n" + usuario, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Error en el formato de la temperatura. Asegúrate de que sea un número válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al guardar los datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void btEncender_Click(object sender, EventArgs e)
+        {
+            if (ArduinoPort.IsOpen)
+            {
+                ArduinoPort.Write("b");
+            }
+        }
+
+        private void CerrandoForm1(object sender, FormClosingEventArgs e)
+        {
+            if (ArduinoPort.IsOpen) ArduinoPort.Close();
+        }
+
+        private void btApagar_Click(object sender, EventArgs e)
+        {
+            if (ArduinoPort.IsOpen)
+            {
+                ArduinoPort.Write("a");
+            }
+        }
+
+    }
+}
+
